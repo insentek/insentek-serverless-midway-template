b26d65243f2df7596cc524b582f00c8c391de23c
dh-free <hao.ding@insentek.com>
2023-05-09 17:23:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# midway quick guide sample
+
+## Usage
+
+```bash
+$ npm i
+$ npm run dev
+$ npm run test
+```
+发布serverless
+
+```bash
+$ npm run deploy
+```
---
+++ src/configuration.ts
@@ -0,0 +1,31 @@
+import { Configuration, App } from '@midwayjs/core';
+import * as koa from '@midwayjs/koa';
+import * as validate from '@midwayjs/validate';
+import * as info from '@midwayjs/info';
+import { join } from 'path';
+import { ReportMiddleware } from './middleware/report.middleware';
+import * as view from '@midwayjs/view-nunjucks';
+
+@Configuration({
+  imports: [
+    koa,
+    validate,
+    {
+      component: info,
+      enabledEnvironment: ['local'],
+    },
+    view,
+  ],
+  importConfigs: [join(__dirname, './config')],
+})
+export class ContainerLifeCycle {
+  @App()
+  app: koa.Application;
+
+  async onReady() {
+    // add middleware
+    this.app.useMiddleware([ReportMiddleware]);
+    // add filter
+    /*    this.app.useFilter([WeatherErrorFilter]);*/
+  }
+}
